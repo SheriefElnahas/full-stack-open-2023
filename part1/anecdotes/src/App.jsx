656e2cd9709298210dfe2153e52723b748ca2eec
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-03-10 17:55:21 +0200

    part1 anecdotes 1.13: step2 votes button

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -4,9 +4,13 @@ const App = () => {
   const anecdotes = ['If it hurts, do it more often.', 'Adding manpower to a late software project makes it later!', 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.', 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', 'Premature optimization is the root of all evil.', 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', 'The only way to go fast, is to go well.'];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
-  // const randomNum = Math.floor(Math.random() * anecdotes.length);
-  // console.log(selected, randomNum);
+  const handleVotes = () => {
+    const newArr = [...votes];
+    newArr[selected] += 1;
+    setVotes(newArr);
+  };
 
   const generateRandomNum = () => {
     setSelected(Math.floor(Math.random() * anecdotes.length));
@@ -14,6 +18,9 @@ const App = () => {
   return (
     <main>
       <p>{anecdotes[selected]}</p>
+      <p>This Anecdotes has {votes[selected]}</p>
+
+      <button onClick={handleVotes}>Vote</button>
       <button onClick={generateRandomNum}>Next Anecdote</button>
     </main>
   );
