6f08c6ca26e81f567cf52b28e245f19310daad86
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-03-15 17:36:23 +0200

    part2 forms 2.7 Phone Book Step2 - Prevent the user from being able to add names that already exist in the phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
+import { useState } from 'react';
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  const [persons, setPersons] = useState([{ name: 'Eren Yeager' }]);
+  const [newName, setNewName] = useState('');
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    persons.find((person) => {
+      if (person.name.toLowerCase() === newName.toLowerCase()) {
+        alert(`${newName} is already added to the phonebook`);
+      }
+    });
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>debug: {newName}</div>
+
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  );
+};
 
 export default App;
