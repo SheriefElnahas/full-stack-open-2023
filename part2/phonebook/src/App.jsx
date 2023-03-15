d137a3c1934dabd676296d0da4261bacf2aad4f7
Sherief Elnahas <sheriefelnahas5@gmail.com>
2023-03-15 17:45:07 +0200

    part2 forms 2.8 Phone Book Step3 - Render users & numbers

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Eren Yeager' }]);
+  const [persons, setPersons] = useState([{ name: 'Eren Yeager', number: '010' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
-  const handleChange = (e) => {
+  const handleNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   const handleSubmit = (e) => {
@@ -21,17 +26,33 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleChange} />
-        </div>
-        <div>debug: {newName}</div>
+        <p>
+          <label htmlFor="name">
+            Name :
+            <input type="text" value={newName} onChange={handleNameChange} />
+          </label>
+        </p>
+        <div>Debug Name: {newName}</div>
+        <p>
+          <label htmlFor="number">
+            Number :
+            <input type="number" value={newNumber} onChange={handleNumberChange} />
+          </label>
+        </p>
+        <div>Debug Number: {newNumber}</div>
 
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => {
+        return (
+          <p key={person.name}>
+            {person.name} {person.number}
+          </p>
+        );
+      })}
     </div>
   );
 };
